e0fe3d016dad86f326e3f1dadd82496dd02fecf9
Frederick Ennin <kpin463@gmail.com>
2022-08-19 11:14:41 +0000

    wedewd

--- menu.php
+++ menu.php
@@ -130,13 +130,53 @@ class Menu
             $quantity = $textlevel[4];
             $totalprice = $productprice * $quantity;
             $res = 'You have ordered '.$quantity.' of '.$productname."\n";
-            $res .= "totalprice = $totalprice \n ";
+            $res .= "Unit Price = $productprice \n ";
+            $res .= "Total Price = $totalprice \n ";
             $res .= "1. Confirm Order\n";
             $res .= "2. Back\n";
             echo 'CON '.$res;
         } elseif ($level == 6) {
             $comfirm = $textlevel[5];
             if ($comfirm == 1) {
+                $product = $textlevel[2];
+                $size = $textlevel[3];
+
+                switch ($product) {
+                case '1':
+                    $productname = 'Pizza';
+                    if ($size == 1) {
+                        $productsize = 'Small';
+                        $productprice = 10;
+                    } elseif ($size == 2) {
+                        $productsize = 'Medium';
+                        $productprice = 20;
+                    } elseif ($size == 3) {
+                        $productsize = 'Large';
+                        $productprice = 40;
+                    }
+                    break;
+
+                case '2':
+                    $productname = 'Burger';
+                    if ($size == 1) {
+                        $productsize = 'Small';
+                        $productprice = 20;
+                    } elseif ($size == 2) {
+                        $productsize = 'Medium';
+                        $productprice = 40;
+                    } elseif ($size == 3) {
+                        $productsize = 'Large';
+                        $productprice = 50;
+                    }
+                    break;
+
+                default:
+                    // code...
+                    break;
+            }
+
+                $quantity = $textlevel[4];
+                $totalprice = $productprice * $quantity;
                 $res = "Order confirmed item will be deliverd to $phoneNumber \n";
 
                 echo 'END '.$res;
